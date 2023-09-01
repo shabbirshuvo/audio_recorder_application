bbabd8506cf75fdd86bd534d36ea26b4dc1f7587
shabbirshuvo <shabbir.unirostock@gmail.com>
2023-09-01 18:55:25 +0200

    final working code

--- main.py
+++ main.py
@@ -4,40 +4,44 @@ from kivymd.app import MDApp
 from kivymd.uix.relativelayout import MDRelativeLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
-from kivy.uix.image import Image
-from threading import Thread
+from threading import Thread, Lock
 import pyaudio
 import wave
 from kivy.core.window import Window
 
-Window.size = (200, 200)
+Window.size = (300, 200)
 
 
 class AudioRecorder(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.lock = Lock()
+        self.recording_active = False
+        self.recording_thread = None
+        self.frames = []
+
     def update_message_label(self, dt):
         text = self.message_label.text
-        updated_text = text[1:] + text[0]  # Move the first character to the end
+        updated_text = text[1:] + text[0]
         self.message_label.text = updated_text
 
-    # text = self.message_label.text
-        # updated_text = text[-1] + text[:-1]  # Move the last character to the beginning
-        # self.message_label.text = updated_text
-
     def record_thread(self, event):
-        self.record_button.text = "Recording..."
-        self.record_button.disabled = True
-        Thread(target=self.record_audio).start()
+        with self.lock:
+            self.record_button.text = "Recording..."
+            self.record_button.disabled = True
+            self.stop_button.disabled = False
+            self.recording_active = True
+        self.frames = []
+        self.recording_thread = Thread(target=self.record_audio)
+        self.recording_thread.start()
 
     def record_audio(self):
         CHUNK = 1024
         FORMAT = pyaudio.paInt16
         CHANNELS = 2
         RATE = 44100
-        RECORD_SECONDS = 10
-        WAVE_OUTPUT_FILENAME = "output.wav"
 
         p = pyaudio.PyAudio()
-
         stream = p.open(
             format=FORMAT,
             channels=CHANNELS,
@@ -46,61 +50,80 @@ class AudioRecorder(MDApp):
             frames_per_buffer=CHUNK,
         )
 
-        frames = []
-
-        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        while self.recording_active:
+            Clock.schedule_once(lambda dt: setattr(self.message_label, 'text', "Recording..."))
             data = stream.read(CHUNK)
-            frames.append(data)
+            self.frames.append(data)
 
         stream.stop_stream()
         stream.close()
         p.terminate()
 
-        wf = wave.open(WAVE_OUTPUT_FILENAME, "wb")
+    def save_audio_to_disk(self):
+        print("Saving to disk...")
+        WAVE_OUTPUT_FILENAME = "output.wav"
+        CHUNK = 1024
+        FORMAT = pyaudio.paInt16
+        CHANNELS = 2
+        RATE = 44100
+
+        wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
         wf.setnchannels(CHANNELS)
-        wf.setsampwidth(p.get_sample_size(FORMAT))
+        wf.setsampwidth(pyaudio.PyAudio().get_sample_size(FORMAT))
         wf.setframerate(RATE)
-        wf.writeframes(b"".join(frames))
+        wf.writeframes(b''.join(self.frames))
         wf.close()
 
+        print("Saved to disk.")
+        Clock.schedule_once(lambda dt: self.update_gui_after_stop())
+
+    def update_gui_after_stop(self):
         self.record_button.text = "Record"
         self.record_button.disabled = False
+        self.message_label.text = "Press Record to start recording....... "
+        self.stop_button.disabled = True
 
     def stop_recording(self, event):
-        self.message_label.text = "Recording stopped"
-        self.stop_button.disabled = True
-        self.record_button.disabled = False
+        with self.lock:
+            self.recording_active = False
+
+        if self.recording_thread and self.recording_thread.is_alive():
+            self.recording_thread.join()
+
+        self.save_audio_to_disk()
 
     def build(self):
         self.root = MDRelativeLayout(md_bg_color=(0.1, 0.1, 0.1, 1))
         self.record_button = Button(
             text="Record",
-            font_size=30,
-            size_hint=(0.3, 0.1),
+            font_size=20,
+            size_hint=(0.4, 0.2),
             pos_hint={"center_x": 0.5, "center_y": 0.7},
             background_color=(1, 0, 0),
         )
         self.record_button.bind(on_press=self.record_thread)
         self.root.add_widget(self.record_button)
+
         self.stop_button = Button(
             text="Stop",
-            font_size=30,
-            size_hint=(0.3, 0.1),
-            pos_hint={"center_x": 0.5, "center_y": 0.5},
+            font_size=20,
+            size_hint=(0.4, 0.2),
+            pos_hint={"center_x": 0.5, "center_y": 0.4},
             background_color=(1, 0, 0),
+            disabled=True,
         )
-        self.root.bind(on_press=self.stop_recording)
+        self.stop_button.bind(on_press=self.stop_recording)
         self.root.add_widget(self.stop_button)
 
         self.message_label = Label(
             text="Press Record to start recording....... ",
-            font_size=40,
-            size_hint=(0.3, 0.1),
-            pos_hint={"center_x": 0.5, "center_y": 0.3},
+            font_size=20,
+            size_hint=(0.8, 0.1),
+            pos_hint={"center_x": 0.5, "center_y": 0.2},
         )
         self.root.add_widget(self.message_label)
 
-        Clock.schedule_interval(self.update_message_label, 0.1)
+        Clock.schedule_interval(self.update_message_label, 0.3)
 
         return self.root
 
